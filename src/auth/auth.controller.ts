1b41fc5f461b2c7115ec7074ac4315aa2e8f4619
Buumba <goldstrangeman@gmail.com>
2024-10-21 11:48:51 +0700

    Initial commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,19 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   // Маршрут для логина пользователя
+  // Используем DTO для валидации данных логина
   @Post('login')
-  async login(@Body() body) {
-    // Валидация пользователя (проверка логина и пароля)
+  async login(@Body() loginDto: LoginDto) {
+    // Валидация пользователя с использованием данных из DTO
     const user = await this.authService.validateUser(
-      body.username,
-      body.password
+      loginDto.username, // Получаем username из DTO
+      loginDto.password // Получаем password из DTO
     );
 
     // Если пользователь не прошёл валидацию, возвращаем ошибку
