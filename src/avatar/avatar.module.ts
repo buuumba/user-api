23ef8ae7cfb3ce3e3484f8f6b357132aba109771
Buumba <goldstrangeman@gmail.com>
2024-11-28 00:19:10 +0700

    Initial commit

--- src/avatar/avatar.module.ts
+++ src/avatar/avatar.module.ts
@@ -1,4 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Avatar } from "./avatar.entity";
+import { AvatarService } from "./avatar.service";
+import { AvatarController } from "./avatar.controller";
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Avatar])],
+  providers: [AvatarService],
+  controllers: [AvatarController],
+})
 export class AvatarModule {}
