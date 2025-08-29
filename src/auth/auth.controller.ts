ff19d935abfb323ccd276d911b601f0caf5741f5
Buumba <goldstrangeman@gmail.com>
2025-08-29 12:37:47 +0300

    auth module refactor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,21 +6,16 @@ import { LoginDto } from "./dto/login.dto";
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // Маршрут для логина пользователя
-  // Используем DTO для валидации данных логина
   @Post("login")
   async login(@Body() loginDto: LoginDto) {
-    // Валидация пользователя с использованием данных из DTO
     const user = await this.authService.validateUser(
-      loginDto.username, // Получаем username из DTO
-      loginDto.password, // Получаем password из DTO
+      loginDto.username,
+      loginDto.password,
     );
 
-    // Если пользователь не прошёл валидацию, возвращаем ошибку
     if (!user) {
       return { error: "Invalid credentials" };
     }
-    // Если валидация успешна, генерируем и возвращаем JWT токен
     return this.authService.login(user);
   }
 }
