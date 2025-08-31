13d35d332e5c01372dcc2252cf40897d0a9eda63
Buumba <goldstrangeman@gmail.com>
2025-08-31 22:29:35 +0300

    avatars upload error fix

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -21,6 +21,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: JwtPayload) {
-    return { userId: payload.sub, username: payload.username };
+    return { id: payload.sub, username: payload.username };
   }
 }
