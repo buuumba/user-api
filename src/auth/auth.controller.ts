abe036070f7d9e43311f85b9ccf42a1a8b68c54a
Buumba <goldstrangeman@gmail.com>
2025-08-31 15:35:18 +0300

    response data fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,14 +3,19 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { LoginResponse } from './interfaces/login-response.interface';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserResponseDto } from '../users/dto/user-response.dto';
+import { toUserResponseDto } from '../users/utils/user-mapper.utils';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() createUserDto: CreateUserDto) {
-    return this.authService.register(createUserDto);
+  async register(
+    @Body() createUserDto: CreateUserDto
+  ): Promise<UserResponseDto> {
+    const user = await this.authService.register(createUserDto);
+    return toUserResponseDto(user);
   }
 
   @Post('login')
